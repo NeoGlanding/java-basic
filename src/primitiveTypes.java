bd1766d9136d5a82c0b9511d43d4ec2846dd142d
NeoGlanding <neoglanding@protonmail.com>
2021-08-20 13:28:01 +0700

    integer in java

---
+++ src/primitiveTypes.java
@@ -0,0 +1,15 @@
+public class primitiveTypes {
+    public static void main (String[] args) {
+        int thisIsNumber = 10;
+        int integerMaxNum = Integer.MAX_VALUE;
+        int integerMinNum = Integer.MIN_VALUE;
+        System.out.println("The max is " + integerMaxNum + ", The min num is " + integerMinNum);
+        System.out.println("The busted value of max is = " + (integerMaxNum + 1));
+        System.out.println("The busted value of min is = " + (integerMinNum - 1));
+
+//        int checkErrorOfMax = 2147483648;
+//        int checkErrorOfMin = -2147483649;
+
+
+    }
+}
